6bf6a58797902bbb6f345c5636ad2ce5a143e84b
hesolar <hectorsolar99@gmail.com>
2024-05-24 00:49:14 +0200

    Inicializar repositorio con un único commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+using CV;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Blazorise;
+using Blazorise.Bootstrap;
+using Blazorise.Icons.FontAwesome;
+using CV.Pages;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<Inicio>("#preRenderComponent");
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+
+builder.Services.AddLocalization();
+
+builder.Services
+    .AddBlazorise(options =>
+    {
+        options.Immediate = true;
+    })
+    .AddBootstrapProviders()
+    .AddFontAwesomeIcons();
+
+
+
+var app = builder.Build();
+await app.RunAsync();
