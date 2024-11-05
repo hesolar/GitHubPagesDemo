568b90a117a707cc1fe76689b3271f5b2cb4a373
d074064 <hector.solar.ruiz@navarra.es>
2024-11-05 12:28:42 +0100

    FUN - Pruebas estilo

--- Program.cs
+++ Program.cs
@@ -1,31 +1,31 @@
-using CV;
-using Microsoft.AspNetCore.Components.Web;
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Blazorise;
 using Blazorise.Bootstrap;
 using Blazorise.Icons.FontAwesome;
+using CV;
 using CV.Pages;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<Inicio>("#preRenderComponent");
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+builder.Services.AddScoped(sp => new HttpClient
+{
+    BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
+});
 
 builder.Services.AddLocalization();
 
-builder.Services
-    .AddBlazorise(options =>
+//Blazorise
+builder
+    .Services.AddBlazorise(options =>
     {
         options.Immediate = true;
     })
     .AddBootstrapProviders()
     .AddFontAwesomeIcons();
-
-
 
 var app = builder.Build();
 await app.RunAsync();
